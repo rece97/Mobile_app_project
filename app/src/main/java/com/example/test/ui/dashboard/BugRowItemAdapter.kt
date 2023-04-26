9a0354ea9cd7e78dd26bf95d1ba9c86951aeb2b3
sharpworks-dev <42219014+sharpworks-dev@users.noreply.github.com>
2023-04-26 13:56:34 -0400

    Further implementation of API. Needs bug fixing on line 46 of DashboardFragment.kt

--- app/src/main/java/com/example/test/ui/dashboard/BugRowItemAdapter.kt
+++ app/src/main/java/com/example/test/ui/dashboard/BugRowItemAdapter.kt
@@ -9,7 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.test.R
 
-class BugRowItemAdapter(private val bugs: Array<Bug>): RecyclerView.Adapter<BugRowItemAdapter.ViewHolder>() {
+class BugRowItemAdapter(private val bugs: ArrayList<Bug>): RecyclerView.Adapter<BugRowItemAdapter.ViewHolder>() {
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         val name = itemView.findViewById<TextView>(R.id.bug_name)
--- app/src/main/java/com/example/test/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/test/ui/dashboard/DashboardFragment.kt
@@ -1,14 +1,17 @@
 package com.example.test.ui.dashboard
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.test.ACNHService
 import com.example.test.R
-import com.example.test.databinding.FragmentDashboardBinding
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
 
 class DashboardFragment : Fragment() {
 
@@ -24,6 +27,42 @@ class DashboardFragment : Fragment() {
 
         val bugList = ArrayList<Bug>()
 
+        val adapter = BugRowItemAdapter(bugList)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.bug_recycler_view)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this.context)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val acnhAPI = retrofit.create(ACNHService::class.java)
+
+        acnhAPI.getAllBugs().enqueue(object : Callback<ArrayList<Bug>> {
+
+
+            override fun onFailure(call: Call<ArrayList<Bug>>, t: Throwable) {
+                Log.d(TAG, "onFailure: $t")
+            }
+
+            override fun onResponse(
+                call: Call<ArrayList<Bug>>,
+                response: Response<ArrayList<Bug>>
+            ) {
+                Log.d(TAG, "onResponse: $response")
+
+                val body = response.body()
+                if(body == null){
+                    Log.w(TAG, "Valid response was not received")
+                    return
+                }
+
+                bugList.addAll(body)
+                adapter.notifyDataSetChanged()
+            }
+            })
 
         return view
     }
